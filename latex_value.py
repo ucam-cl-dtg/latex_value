3c36927df5caa044cf127d0e0ddfcc009576ba9d
Daniel Thomas <drt24@cam.ac.uk>
2015-09-28 11:02:23 +0100

    Million etc. should not be capitalised

--- latex_value.py
+++ latex_value.py
@@ -171,7 +171,7 @@ def round_num(num, sig_figs):
 
 def num2word(n):
     # https://stackoverflow.com/questions/3154460/python-human-readable-large-numbers/3155023#3155023
-    millnames = ['', 'Thousand', 'Million', 'Billion', 'Trillion']
+    millnames = ['', 'thousand', 'million', 'billion', 'trillion']
     n = float(n)
     millidx = max(0, min(len(millnames) - 1, int(floor(log10(abs(n)) / 3))))
     return '%.0f %s' % (n / 10 ** (3 * millidx), millnames[millidx])
